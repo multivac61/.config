f0391a8c620b3451d8d406e1414b4d083e6eed24
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2023-05-12 10:12:22 +0000

    Add nvim folder...

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,20 @@
+return {
+    { "catppuccin/nvim", name = "catppuccin", lazy = true },
+    -- {
+    --   "LazyVim/LazyVim",
+    --   opts = {
+    --     colorscheme = "catppuccin-mocha",
+    --   },
+    -- },
+    {
+        "LazyVim/LazyVim",
+        opts = {
+            colorscheme = "catppuccin-mocha",
+        },
+    },
+    --disable default colorscheme
+    -- {
+    --   "folke/tokyonight.nvim",
+    --   lazy = true,
+    -- },
+}
---
+++ nvim/lua/plugins/svelte.lua
@@ -0,0 +1,21 @@
+return {
+
+    -- add svelte to treesitter
+    {
+        "nvim-treesitter/nvim-treesitter",
+        opts = function(_, opts)
+            vim.list_extend(opts.ensure_installed, { "svelte", "prisma" })
+        end,
+    },
+
+    -- correctly setup mason lsp / dap extensions
+    {
+        "williamboman/mason.nvim",
+        opts = function(_, opts)
+            vim.list_extend(
+                opts.ensure_installed,
+                { "svelte-language-server", "prisma-language-server", "tailwindcss-language-server" }
+            )
+        end,
+    },
+}
